e6f45c0759b2a240095715521b68e1e9111545f0
Gulraphat <gulraphat.sw@gmail.com>
2023-06-23 16:04:40 +0700

    ติดปัญหาการรับรูปภาพ

--- graphql-api/index.js
+++ graphql-api/index.js
@@ -1,11 +1,12 @@
-const { ApolloServer} = require('apollo-server-express');
-const { graphqlUploadExpress } = require('graphql-upload');
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+import { ApolloServer } from 'apollo-server-express';
+// import { GraphQLUpload } from 'graphql-upload';
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import mysql from 'mysql2/promise';
 
 async function main() {
-    const mysql = require('mysql2/promise');
+    
     const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'graphql_shop' });
     const [rows, fields] = await connection.execute('SELECT * FROM `items`');
 
@@ -31,8 +32,7 @@ async function main() {
         }
 
         type Mutation {
-            createItem(name: String!, category_id: Int!, detail: String!, image: Upload!, price: Int!): Item
-            updateItem(id: Int!, name: String!, category_id: Int!, detail: String!, image: String!, price: Int!): Item
+            # createItem(name: String!, category_id: Int!, detail: String!, image: Upload!, price: Int!): Item
             deleteItem(id: Int!): Item
             createCategory(name: String!): Category
             updateCategory(id: Int!, name: String!): Category
@@ -41,7 +41,7 @@ async function main() {
     `;
 
     const resolvers = {
-        Upload: graphqlUploadExpress,
+        Upload: GraphQLUpload,
 
         Query: {
             categories: async () => {
@@ -75,34 +75,23 @@ async function main() {
             }
         },
         Mutation: {
-            createItem: async (parent, { name, category_id, detail, image, price }) => {
-                const { createReadStream, filename, mimetype, encoding } = await image;
-                const stream = createReadStream();
-                const pathName = path.join(__dirname, `/public/image/${filename}`);
-                await stream.pipe(fs.createWriteStream(pathName));
-                imagePath = `http://localhost:4000/image/${filename}`;
+            // createItem: async (parent, { name, category_id, detail, image, price }) => {
+            //     const { createReadStream, filename, mimetype, encoding } = await image;
+            //     const stream = createReadStream();
+            //     const pathName = path.join(__dirname, `/public/image/${filename}`);
+            //     await stream.pipe(fs.createWriteStream(pathName));
+            //     imagePath = `http://localhost:4000/image/${filename}`;
 
-                const [rows, fields] = await connection.execute(
-                    `INSERT INTO items (name, category_id, detail, image, price) VALUES (?, ?, ?, ?, ?)`
-                    , [name, category_id, detail, imagePath, price]
-                );
-                const [rows2, fields2] = await connection.execute(
-                    `SELECT * FROM items WHERE id = ?`
-                    , [rows.insertId]
-                );
-                return rows2[0];
-            },
-            updateItem: async (parent, { id, name, category_id, detail, image, price }) => {
-                const [rows, fields] = await connection.execute(
-                    `UPDATE items SET name = ?, category_id = ?, detail = ?, image = ?, price = ? WHERE id = ?`
-                    , [name, category_id, detail, image, price, id]
-                );
-                const [rows2, fields2] = await connection.execute(
-                    `SELECT * FROM items WHERE id = ?`
-                    , [id]
-                );
-                return rows2[0];
-            },
+            //     const [rows, fields] = await connection.execute(
+            //         `INSERT INTO items (name, category_id, detail, image, price) VALUES (?, ?, ?, ?, ?)`
+            //         , [name, category_id, detail, imagePath, price]
+            //     );
+            //     const [rows2, fields2] = await connection.execute(
+            //         `SELECT * FROM items WHERE id = ?`
+            //         , [rows.insertId]
+            //     );
+            //     return rows2[0];
+            // },
             deleteItem: async (parent, { id }) => {
                 const [rows, fields] = await connection.execute(
                     `DELETE FROM items WHERE id = ?`
