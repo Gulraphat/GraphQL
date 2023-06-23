5a34ae8aafd620fafc98ba8c7a88a74322b50f33
Gulraphat <gulraphat.sw@gmail.com>
2023-06-23 13:46:04 +0700

    เพิ่ม category

--- graphql-api/index.js
+++ graphql-api/index.js
@@ -10,12 +10,19 @@ async function main() {
         type Item {
             id: Int
             name: String
+            category: Category
             detail: String
             image: String
             price: Int
         }
+    
+        type Category {
+            id: Int
+            name: String
+        }
 
-        type Query {
+       type Query {
+            categories: [Category]
             items: [Item]
             item(id: Int!): Item 
         }
@@ -23,12 +30,29 @@ async function main() {
 
     const resolvers = {
         Query: {
+            categories: async () => {
+                const [rows, fields] = await connection.execute('SELECT * FROM `categories`');
+                return rows;
+            },
             items: async () => {
-                const [rows, fields] = await connection.execute('SELECT * FROM `items`');
+                const [rows, fields] = await connection.execute(`SELECT * FROM items` );
                 return rows;
             },
             item: async (parent, { id }) => {
-                const [rows, fields] = await connection.execute('SELECT * FROM `items` WHERE `id` = ?', [id]);
+                const [rows, fields] = await connection.execute(`SELECT * FROM items WHERE id = ?`, [id]);
+                if (rows.length > 0) {
+                    return rows[0];
+                } else {
+                    return null;
+                }
+            }
+        },
+        Item: {
+            category: async (parent) => {
+                const [rows, fields] = await connection.execute(
+                    `SELECT * FROM categories WHERE id = ?`
+                    , [parent.category_id]
+                );
                 if (rows.length > 0) {
                     return rows[0];
                 } else {
@@ -40,7 +64,7 @@ async function main() {
 
     const server = new ApolloServer({
         typeDefs,
-        resolvers,
+        resolvers,   
     });
 
     const app = express();
--- shop.sql
+++ shop.sql
@@ -5,22 +5,38 @@ SET time_zone = "+00:00";
 CREATE TABLE `items` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
+  `category_id` int(11) NOT NULL,
   `detail` varchar(500) NOT NULL,
   `image` varchar(255) NOT NULL,
   `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`),
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-INSERT INTO `items` (`name`, `detail`, `image`, `price`) VALUES
-('First Cat', 'meow meow', 'http://localhost:4000/image/1.jpg', '1000'),
-('Second Cat', 'meow!', 'http://localhost:4000/image/2.jpg', '2000'),
-('Third Cat', 'meoooooooow', 'http://localhost:4000/image/3.jpg', '1500'),
-('Fourth Cat', 'meow meow meow!', 'http://localhost:4000/image/4.jpg', '1000'),
-('Fifth Cat', 'meooooooooow meow!', 'http://localhost:4000/image/5.jpg', '2500');
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO `categories` (`name`) VALUES
+('Cat'),
+('Dog'),
+('Rabbit'),
+('Bird'),
+('Fish');
+
+INSERT INTO `items` (`name`, `category_id`, `detail`, `image`, `price`) VALUES
+('First Cat', 1, 'meow meow', 'http://localhost:4000/image/1.jpg', '1000'),
+('Second Cat', 1, 'meow!', 'http://localhost:4000/image/2.jpg', '2000'),
+('Third Cat', 1, 'meoooooooow', 'http://localhost:4000/image/3.jpg', '1500'),
+('Fourth Cat', 1, 'meow meow meow!', 'http://localhost:4000/image/4.jpg', '1000'),
+('Fifth Cat', 1, 'meooooooooow meow!', 'http://localhost:4000/image/5.jpg', '2500');
 
 COMMIT;
